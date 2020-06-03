19ac35bb394702ab3425fb063794802616d8ea04
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-06-03 20:51:32 +0200

    Fix house id retrieval

--- app/user_task.py
+++ app/user_task.py
@@ -8,8 +8,7 @@ user_task_blueprint = Blueprint("user_task", __name__)
 @user_task_blueprint.route("/task/<id>/user_task", \
                            methods=("GET",))
 def get_task(identifier):
-    user_task: UserTask = UserTask.query.get(identifier)
-    ).first()
+    user_task: UserTask = UserTask.query.get(identifier)).first()
 
     if not user_task:
         return jsonify({"msg": "A user_task with that identifier does not exist."}), 403
@@ -18,6 +17,9 @@ def get_task(identifier):
     data["user_id"] = user_task.user_id
     data["task_id"] = user_task.task_id
     data["deadline"] = user_task.deadline
-    data["house_id"] = user_task.house_id
+
+    # Get house id from UserHouse using user_id
+    user_house: UserHouse = UserHouse.query.filter(user_id == user_task.user_id)).first()
+    data["house_id"] = user_house.house_id
 
     return jsonify(data=data), 200
