eba777e8429236029895bb93d812bd81c4626dec
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-03 13:42:06 +0100

    Remove `else`

--- app/utils.py
+++ app/utils.py
@@ -4,8 +4,7 @@ from flask import jsonify
 def error_missing_json_key(*args):
     if len(args) == 1:
         return jsonify({"msg": "Missing key `{}`".format(args[0]), "status": "error"}), 400
-    else:
-        return jsonify({"msg": "Missing a JSON key of name " + ", ".join(map(lambda x: "`{}`".format(x),
-                                                                             args[:-1]
-                                                                             )) + " or `{}`".format(args[-1]),
-                        "status": "error"}, )
+    return jsonify({"msg": "Missing a JSON key of name " + ", ".join(map(lambda x: "`{}`".format(x),
+                                                                         args[:-1]
+                                                                         )) + " or `{}`".format(args[-1]),
+                    "status": "error"}, )
