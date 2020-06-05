5999298e68d3b6930b1660c0944703aa4ecd4d3b
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-05 14:13:06 +0100

    Remove old worker stuff.

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,8 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from rq import Queue
 from flask_mail import Mail
 
-from worker import conn
 
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
@@ -18,7 +16,6 @@ sentry_sdk.init(
     integrations=[FlaskIntegration()],
 )
 
-q = Queue(connection=conn)
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
